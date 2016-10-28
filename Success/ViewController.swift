a2993fa54f4798ce86a3752eb047338dbbfaccba
Jared Lindsay <director@bmadojo.com>
2016-10-28 14:55:10 -0700

    initial commit. everything works, but it's not terribly pretty. could use some coloring to make things easier to read.

--- Success/ViewController.swift
+++ Success/ViewController.swift
@@ -9,6 +9,9 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet weak var rollView: NSTextField!
+    @IBOutlet weak var resultsView: NSTextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,85 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func rollDice(_ sender: NSMatrix) {
+        let dicePool = sender.selectedCell()!.tag
+        var rolls = [Int]()
+        
+        for _ in 1...dicePool {
+            rolls.append(Int(arc4random_uniform(10) + 1))
+        }
+        
+        displayRolls(rolls: rolls)
+        calculateSuccesses(rolls: rolls)
+    }
+    
+    func displayRolls(rolls: [Int]) {
+        var output = ""
+        var current = 1
+        
+        for roll in rolls {
+            output = "\(output)\(roll)"
+            if current < 8 {
+                output = "\(output)\t"
+            }
+            else {
+                output = "\(output)\n"
+                current = 1
+            }
+            current += 1
+        }
+        self.rollView.stringValue = output
+    }
+    
+    func calculateSuccesses(rolls: [Int]) {
+        var finalOutput = ""
+        var numTens = 0
+        
+        for roll in rolls {
+            if roll == 10 {
+                numTens += 1
+            }
+        }
+        
+        for difficulty in 3...10 {
+            var successes = 0
+            var botches = 0
+            
+            for roll in rolls {
+                if roll >= difficulty {
+                    successes += 1
+                }
+                else if roll == 1 {
+                    botches += 1
+                }
+            }
+            
+            let specialtySuccesses = successes + numTens
+            var output = "\(difficulty):"
+            
+            if successes == 0 && botches >= 1 {
+                output = "\(output)\tBOTCH"
+            }
+            else if botches >= successes {
+                output = "\(output)\tFailure"
+                
+                if numTens > 0 {
+                    if botches < specialtySuccesses {
+                        output = "\(output)\t\t(\(specialtySuccesses - botches))"
+                    }
+                }
+            }
+            else {
+                output = "\(output)\t\(successes - botches)"
+                if numTens > 0 {
+                    output = "\(output)\t\t\t(\(specialtySuccesses - botches))"
+                }
+            }
+            
+            finalOutput = "\(finalOutput)\(output)\n"
+        }
+        self.resultsView.stringValue = finalOutput
+    }
 
 }
 
